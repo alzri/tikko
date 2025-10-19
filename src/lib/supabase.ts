5d15b1852f0855e7af63a5755307a18e47f32216
azrinka <alzrinka1@gmail.com>
2025-10-19 14:15:09 +0200

    config

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@/node_modules/@supabase/supabase-js/dist/module/index';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
