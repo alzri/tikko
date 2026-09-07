07764f9280e62b369e4034b537586c722cf4a1b7
azrinka <alzrinka1@gmail.com>
2026-09-07 19:47:25 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,16 @@
-/* const isProd = process.env.NODE_ENV === 'production'; */
-
 const nextConfig = {
   images: { unoptimized: true },
   sassOptions: {
     includePaths: ['./styles'],
   },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    // camel-case style names from css modules
-    config.module.rules
-      .find(({ oneOf }) => !!oneOf)
-      .oneOf.filter(({ use }) => JSON.stringify(use)?.includes('css-loader'))
-      .reduce((acc, { use }) => acc.concat(use), [])
-      .forEach(({ options }) => {
-        if (options.modules) {
-          options.modules.exportLocalsConvention = 'camelCase';
-        }
-      });
-    return config;
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
   },
 };
 
